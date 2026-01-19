c5294fe1cbc4fed7c4964ce2eac0ceca533b9173
V C Adith <adithchandrasekar@gmail.com>
2026-01-19 15:25:06 +0530

    Fix CORS with manual headers for Vercel serverless

--- server.js
+++ server.js
@@ -8,13 +8,21 @@ const { User, UserData } = require('./models');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: '*',
-  credentials: false,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(bodyParser.json());
+
+// Add CORS headers middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
